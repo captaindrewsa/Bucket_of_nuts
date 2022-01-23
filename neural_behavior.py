6f7bc8a88749fc154990fbeed026068c3100cb7b
iam-kg <chonpsthetent@gmail.com>
2022-01-23 23:15:24 +0200

    Update neural_behavior.py

--- neural_behavior.py
+++ neural_behavior.py
@@ -14,7 +14,11 @@ class Neuron:
     def __init__(self, num_inputs, matrixsize):
         self.num_inputs = num_inputs # Ammount of inputs, int
         param = Param(matrixsize)
-        self.value, self.electricity = param.numbers(0.001 * np.random.randint(-10,10,matrixsize))
+        self.value, self.electricity = param.numbers(0.01 * np.random.randint(-10,10,matrixsize))
+
+        self.defaults = param.numbers(0.01 * np.random.randint(-10,10,matrixsize)) #default properties of a neuron, cannot be changed during transmission
+
+
 
     def accumulation(self, inputs):
 
@@ -30,17 +34,42 @@ class Neuron:
         return
 
 
+    def fire(self):
+        '''
+        Neural output
+        
+        returns:
+        fire - does this neuron fire now or not - bool
+        answer - kind of fire, inhibitory or excitatory - np.array (matrixsize)
+        '''
+        if self.electricity < 1 and self.electricity >-1:
+            fire = False
+            answer = None
+        else:
+            fire = True
+            answer = self.value
+        return fire, answer
+
+
+    def exhaust(self):
+        '''
+        Neural recovery, if transmission happened
+        '''
+        pass
+
+
     def params(self):
         return {'value': self.value, 'electricity': self.electricity}
 
 
 
 #a = np.arange(9) + 1.25
-a = np.random.randint(-10,10,(3,3))
+a = np.random.randint(-10,10,(3,3)) *0.01
 a = np.reshape(a, (3,3))
 print(np.linalg.det(a))
 a = np.array([a-1,a-3,a-2.3,a+1.2,a])
 
 neuron = Neuron(a.shape[0], (3,3))
 neuron.accumulation(a)
+
 print(neuron.params())
