8249a65e4141591564112a1bcf843e8de692438a
captaindrewsa <captaindrewsa@gmai.com>
2022-02-02 18:51:38 +0200

    MVP модель, которая только создает связную матрицу со случайными связями

--- HyperSlavs/Matrix.py
+++ HyperSlavs/Matrix.py
@@ -1,5 +1,6 @@
 import numpy as np
 from Node import Node
+from tqdm import tqdm
 
 class Matrix:
     '''Класс ядер. Заниамет созданием и можифидацией Kernel'сов. Настраивается в основном в ручную,
@@ -15,11 +16,12 @@ class Matrix:
         self.matrix = self._create_matrix()
         '''Переменная для доступа к матрице'''
         self._add_Kernel_n()
+        self._add_Kernel_p()
     
     def _create_matrix(self):
         '''Создает матрицу заданных размеров и заполняет ее пустыми нодами'''
         matrix = np.zeros((self.size_matrix,self.size_matrix), dtype= object)
-        for idx1 in range(self.size_matrix):
+        for idx1 in tqdm(range(self.size_matrix)):
             matrix[idx1] = [Node() for _ in range(self.size_matrix)]
         return matrix
 
@@ -29,21 +31,24 @@ class Matrix:
         Kernel_n = np.array([[np.random.randint(self.size_matrix, dtype=np.int64),np.random.randint(self.size_matrix, dtype=np.int64),round(np.random.random(), 3)] for _ in range(count)])
         return Kernel_n
 
-    def _create_Kernel_p(self, noda: Node, xy_noda: list):
+    def _append_Kernel_p(self, noda: Node, xy_noda: list):
         '''Достает из ноды координты и добавляет данные в другие ноды'''
         for coordinates in noda.Kernel_n[0,:,:]:
-            if np.sum(self.matrix[tuple(np.array(coordinates, dtype=np.int64))]) == 0.5:
-                pass
-                    
+            coordinates = tuple(np.array(coordinates, dtype=np.int64))[:2]
+            if isinstance(self.matrix[coordinates].Kernel_p, int):
+                self.matrix[coordinates].Kernel_p = np.array([xy_noda])
+            else:
+                self.matrix[coordinates].Kernel_p = np.append(self.matrix[coordinates].Kernel_p, np.array([xy_noda]), axis=0)
 
 
     def _add_Kernel_n(self):
         '''Пробегает по нодам и добавляет им Kernel_n'''
-        for row in self.matrix:
+        for row in tqdm(self.matrix):
             for elem in row:
                 elem.Kernel_n = np.stack(np.array([self._create_Kernel_n()]))
     
     def _add_Kernel_p(self):
         '''Заполняет нодам Kernel_p на основе связей из Kernel_n'''
-        pass
-
+        for idx1, row in tqdm(enumerate(self.matrix)):
+            for idx2, elem in enumerate(row):
+                self._append_Kernel_p(elem, [idx1,idx2])
